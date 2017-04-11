e95002c1c4cd60ac6370b8112c3e075be5b5bf21
flightgod <flightgod@gmail.com>
2017-04-10 19:05:25 -0500

    updating

---
+++ duino/fc_led_matrix_evil_eyes/fc_led_matrix_evil_eyes.ino
@@ -0,0 +1,299 @@
+// Evil Eyes
+// Art Dahm, <art@dahm.com>
+// http://dragongears.com
+//
+// Modified to use two LED matrix link boards and the LED matrix master from
+// FriedCircuits (http://friedcircuits.us/) instead of the original
+// Adafruit Bicolor LED Square Pixel Matrix with I2C Backpack although any
+// Arduino compatible board with MAX7219 LED Display Driver ICs should work
+//
+// Based on Halloween Eyes
+// Brian Enigma, <brian@netninja.com>
+// http://nja.me/eyes
+//
+// Based in part on the "Roboface" code from Adafruit:
+// https://github.com/adafruit/Adafruit-LED-Backpack-Library/blob/master/examples/roboface/roboface.pde
+//
+// ----------------------------------------
+//
+// Adafruit invests time and resources providing this open source code,
+// please support Adafruit and open-source hardware by purchasing
+// products from Adafruit!
+//
+// Written by P. Burgess for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <avr/pgmspace.h>
+#include <LedControl.h>
+
+#define LED_ON 1
+#define LED_OFF 0
+
+#define RIGHT_EYE 0
+#define LEFT_EYE  1
+#define PUPIL_Y 4
+#define PUPIL_WIDTH 2
+#define PUPIL_HEIGHT 2
+#define RIGHT_PUPIL_X_ADJUST -2
+
+// Initialize LED Controllers
+
+const int devCount = 2;
+int brightness = 15;
+
+int ledPin = 13;
+
+LedControl lc=LedControl(12,11,10,devCount);
+
+uint8_t eyeMatrix[2][8];
+
+// Eye bitmaps array
+
+static const uint8_t PROGMEM // Bitmaps are stored in program memory
+blinkImg[][8] = {    // Eye animation frames
+    // The EVIL (red) eye, left
+    { B00000000,         // Fully open evil/red eye
+        B11000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00011100 },
+    { B00000000,
+        B00000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00011100 },
+    { B00000000,
+        B00000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00000000 },
+    { B00000000,
+        B00000000,
+        B00000000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00000000,
+        B00000000 },
+    { B00000000,         // Fully closed evil/red eye
+        B00000000,
+        B00000000,
+        B00000000,
+        B01111111,
+        B00000000,
+        B00000000,
+        B00000000 },
+    // The EVIL (red) eye, right
+    { B00000000,         // Fully open evil/red eye
+        B00000011,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00111000 },
+    { B00000000,
+        B00000000,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00111000 },
+    { B00000000,
+        B00000000,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00000000 },
+    { B00000000,
+        B00000000,
+        B00000000,
+        B11111110,
+        B11111110,
+        B11111100,
+        B00000000,
+        B00000000 },
+    { B00000000,         // Fully closed evil/red eye
+        B00000000,
+        B00000000,
+        B00000000,
+        B11111110,
+        B00000000,
+        B00000000,
+        B00000000 }
+};
+
+// Offset to the right eye in the eye array
+#define RIGHT_EYE_OFFSET (8 * 5)
+
+// Initialize blink and gaze variables
+uint8_t
+	blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
+    blinkCountdown = 100, // Countdown to next blink (in frames)
+    gazeCountdown  =  75, // Countdown to next eye movement
+    gazeFrames     =  50; // Duration of eye movement (smaller = faster)
+int8_t
+    eyeX = 3,   // Current eye position
+    newX = 3,   // Next eye position
+    dX   = 0;   // Distance from prior to new position
+
+// Draw a pixel
+void drawPixel(uint8_t *displaybuffer, int16_t x, int16_t y, uint8_t color) {
+	if ((y < 0) || (y >= 8)) return;
+	if ((x < 0) || (x >= 8)) return;
+
+	x = 7-x;
+
+	if (color) {
+		displaybuffer[y] |= 1 << x;
+	} else {
+		displaybuffer[y] &= ~(1 << x);
+	}
+}
+
+// Draw a vertical line
+void drawVLine(uint8_t *displaybuffer, int16_t y, int16_t x, int16_t w, uint8_t color) {
+	for (int16_t i=x; i<x+w; i++) {
+		drawPixel(displaybuffer, i, y, color);
+	}
+}
+
+// Fill a rectangle with a color
+void fillRect(uint8_t *displaybuffer, int16_t x, int16_t y, int16_t w, int16_t h, uint8_t color) {
+	for (int16_t i=y; i<y+h; i++) {
+		drawVLine(displaybuffer, i, x, w, color);
+	}
+}
+
+// Clear display buffer
+void clear(uint8_t *displaybuffer) {
+	for (uint8_t i=0; i<8; i++) {
+		displaybuffer[i] = 0;
+	}
+}
+
+// Copy bitmap from array to display buffer
+void drawBitmap(uint8_t *displaybuffer, const uint8_t *source) {
+	for (uint8_t i=0; i<8; i++) {
+		displaybuffer[i] = pgm_read_byte(&source[i]);
+	}
+}
+
+// Write display buffer to LED matrix
+void writeDisplay(uint8_t *displaybuffer, uint8_t out) {
+	for (uint8_t i=0; i<8; i++) {
+		lc.setRow(out, i, displaybuffer[i]);
+	}
+}
+
+// Main setup
+void setup() {
+
+	// Seed random number generator from an unused analog input:
+	randomSeed(analogRead(A0));
+
+	// Initialize LED displays
+	for(int address=0;address<devCount;address++) {
+		/*The MAX72XX is in power-saving mode on startup*/
+		lc.shutdown(address,false);
+		/* Set the brightness to a medium values */
+		lc.setIntensity(address,brightness);
+		/* and clear the display */
+		lc.clearDisplay(address);
+	}
+}
+
+// Main loop
+void loop()
+{
+    uint8_t motion;
+    const uint8_t *imageOffset;
+    // Draw eyeball in current state of blinkyness (no pupil).
+    clear(&eyeMatrix[LEFT_EYE][0]);
+    clear(&eyeMatrix[RIGHT_EYE][0]);
+
+    // When counting down to the next blink, show the eye in the fully-
+    // open state.  On the last few counts (during the blink), look up
+    // the corresponding bitmap index.
+    imageOffset = 
+            &blinkImg[
+            (blinkCountdown < sizeof(blinkIndex)) ? // Currently blinking?
+            blinkIndex[blinkCountdown] :            // Yes, look up bitmap #
+            0                                       // No, show bitmap 0
+            ][0];
+    drawBitmap(&eyeMatrix[LEFT_EYE][0], imageOffset);
+    imageOffset += RIGHT_EYE_OFFSET;
+    drawBitmap(&eyeMatrix[RIGHT_EYE][0], imageOffset);
+
+    // Decrement blink counter.  At end, set random time for next blink.
+    blinkCountdown--;
+    if (blinkCountdown == 0) {
+        blinkCountdown = random(5, 180);
+    }
+
+    // Add a pupil (2x2 black square) atop the blinky eyeball bitmap.
+    // Periodically, the pupil moves to a new position...
+    if(--gazeCountdown <= gazeFrames) {
+        // Eyes are in motion - draw pupil at interim position
+        fillRect(
+                &eyeMatrix[LEFT_EYE][0], 
+                newX - (dX * gazeCountdown / gazeFrames),
+                PUPIL_Y,
+                PUPIL_WIDTH,
+                PUPIL_HEIGHT,
+                LED_OFF);
+        fillRect(
+                &eyeMatrix[RIGHT_EYE][0], 
+                newX - (dX * gazeCountdown / gazeFrames) + RIGHT_PUPIL_X_ADJUST,
+                PUPIL_Y,
+                PUPIL_WIDTH,
+                PUPIL_HEIGHT,
+                LED_OFF);
+        if(gazeCountdown == 0) {    // Last frame?
+            eyeX = newX; // Yes.  What's new is old, then...
+            newX = random(3) + 3;
+            dX            = newX - eyeX;             // Horizontal distance to move
+            gazeFrames    = random(3, 15);           // Duration of eye movement
+            gazeCountdown = random(gazeFrames, 120); // Count to end of next movement
+        }
+    } else {
+        // Not in motion yet -- draw pupil at current static position
+        fillRect(
+                &eyeMatrix[LEFT_EYE][0],
+                eyeX,
+                PUPIL_Y,
+                PUPIL_WIDTH,
+                PUPIL_HEIGHT,
+                LED_OFF);
+        fillRect(
+                &eyeMatrix[RIGHT_EYE][0],
+                eyeX + RIGHT_PUPIL_X_ADJUST,
+                PUPIL_Y,
+                PUPIL_WIDTH,
+                PUPIL_HEIGHT,
+                LED_OFF);
+    }
+
+    // Refresh all of the matrices in one quick pass
+    writeDisplay(&eyeMatrix[LEFT_EYE][0], LEFT_EYE);
+    writeDisplay(&eyeMatrix[RIGHT_EYE][0], RIGHT_EYE);
+
+ 
+
+
+    delay(20); // ~50 FPS
+
+
+}
---
+++ libraries/car/car/car.ino
@@ -0,0 +1,109 @@
+/**************************************************************
+ * This example runs directly on ESP8266 chip
+ * using the Blynk platform and mobile application.
+ *
+ * Change WiFi ssid, password, and Blynk auth token to run :)
+ *
+ **************************************************************/
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define RightMotorSpeed 5
+#define RightMotorDir   0
+#define LeftMotorSpeed  4
+#define LeftMotorDir    2
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = “95071edf022f4318815725047fb59642”;
+char ssid[] = “BennNeet”;
+char pass[] = “River11111”;
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+
+  pinMode(RightMotorSpeed, OUTPUT);
+  pinMode(RightMotorDir, OUTPUT);
+  pinMode(LeftMotorSpeed, OUTPUT);
+  pinMode(LeftMotorDir, OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+}
+
+void halt()
+{
+  digitalWrite(RightMotorSpeed, LOW);
+  digitalWrite(LeftMotorSpeed, LOW);
+}
+
+void forward()
+{
+  digitalWrite(RightMotorDir, HIGH);
+  digitalWrite(LeftMotorDir, HIGH);
+  digitalWrite(RightMotorSpeed, HIGH);
+  digitalWrite(LeftMotorSpeed, HIGH);
+}
+
+void reverse()
+{
+  digitalWrite(RightMotorDir, LOW);
+  digitalWrite(LeftMotorDir, LOW);
+  digitalWrite(RightMotorSpeed, HIGH);
+  digitalWrite(LeftMotorSpeed, HIGH);
+}
+
+void right()
+{
+  digitalWrite(RightMotorDir, LOW);
+  digitalWrite(LeftMotorDir, HIGH);
+  digitalWrite(RightMotorSpeed, HIGH);
+  digitalWrite(LeftMotorSpeed, HIGH);
+}
+
+void left()
+{
+  digitalWrite(RightMotorDir, HIGH);
+  digitalWrite(LeftMotorDir, LOW);
+  digitalWrite(RightMotorSpeed, HIGH);
+  digitalWrite(LeftMotorSpeed, HIGH);
+}
+
+BLYNK_WRITE(V0)
+{
+  if (param[0])
+    forward();
+  else
+    halt();
+}
+
+BLYNK_WRITE(V1)
+{
+  if (param[0])
+    reverse();
+  else
+    halt();
+}
+
+BLYNK_WRITE(V2)
+{
+  if (param[0])
+    right();
+  else
+    halt();
+}
+
+BLYNK_WRITE(V3)
+{
+  if (param[0])
+    left();
+  else
+    halt();
+}
