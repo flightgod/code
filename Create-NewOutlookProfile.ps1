ed2a900d21c7f172570ff78aada2fd93c16856e9
Kevin Bennett <flightgod@gmail.com>
2017-10-23 10:36:50 -0500

    Added for testing
    added for PS webclient testing

---
+++ Create-NewOutlookProfile.ps1
@@ -0,0 +1,33 @@
+﻿# variables
+$reg="HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles"
+$child=(Get-ChildItem -Path $reg).name
+
+Function DeleteProfiles {
+    Write-Host "All profiles removed successfully" -ForegroundColor Green
+    foreach($item in $child)
+    {
+        Remove-item -Path registry::$item -Recurse #-ErrorAction Inquire -WhatIf
+    }
+}
+
+Function CloseOutlook {
+    if($process=(get-process 'outlook' -ErrorAction SilentlyContinue))
+    {
+        Write-Host "Outlook is running so close it.." -ForegroundColor Green
+        kill($process)
+        Write-Host "Outlook is stopped " -ForegroundColor Green
+    }
+}
+
+Function CreateBlankProfile {
+    Write-Host "Now create new profile for outlook" -ForegroundColor Green
+    New-Item -Name "NewCrap" -Path $reg -Force -Verbose
+}
+
+
+Function OpenNewProfile {
+Write-Host "Launch outlook with newly created profile" -ForegroundColor Green
+    Start-Process 'outlook' -ErrorAction SilentlyContinue -ArgumentList '/profile "NewCrap" '
+}
+
+CloseOutlook
